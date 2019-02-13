65fe1d28050dc0c7cfafb9dbc9148133797cd0dc
David Thayer <dthayer16@gmail.com>
2019-02-13 19:10:07 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -16,7 +16,11 @@ def times_iterator(number_of_times)
 end
 
 def while_iterator(number_of_times)
-
+  counter = 0
+  while counter < 7
+    puts "Welcome to Flatiron School's Web Development Course!"
+  counter += 1
+  end
 end
 
 def until_iterator(number_of_times)
